89d40487dd897b425d2d591118769bd51477a55c
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-09 12:05:57 +0100

    Add author information to plugin header

--- lean-stats.php
+++ lean-stats.php
@@ -3,6 +3,7 @@
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
  * Version: 0.10.0
+ * Author: BimBeau
  * Author: Lean Stats
  * Requires at least: 6.4
  * Requires PHP: 8.0
