97d26cb0b6e6b9eb4528351b41b67a445dfe32a5
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-10 18:06:08 +0100

    Improve npm build reproducibility (#37)

--- lean-stats.php
+++ lean-stats.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Lean Stats
  * Description: Privacy-friendly, self-hosted analytics for WordPress.
- * Version: 0.13.3
+ * Version: 0.13.4
  * Author: BimBeau
  * Author: Lean Stats
  * Requires at least: 6.4
