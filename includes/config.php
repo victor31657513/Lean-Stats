78ef82690363ce02dac4ce12b2e053d9573ef320
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-11 10:41:42 +0100

    Add Dutch translation

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@
 defined('ABSPATH') || exit;
 
 return [
-    'version' => '0.17.0',
+    'version' => '0.18.0',
     'slug' => 'lean-stats',
     'rest_namespace' => 'lean-stats/v1',
     'rest_namespace_internal' => 'lean-stats/internal/v1',
