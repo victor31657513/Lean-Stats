3900807821a5475430efde0fa5befec2277eb92a
Victor <78041023+victor31657513@users.noreply.github.com>
2026-01-11 10:23:16 +0100

    Update deploy workflow and bump version (#54)

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@
 defined('ABSPATH') || exit;
 
 return [
-    'version' => '0.15.0',
+    'version' => '0.16.1',
     'slug' => 'lean-stats',
     'rest_namespace' => 'lean-stats/v1',
     'rest_namespace_internal' => 'lean-stats/internal/v1',
